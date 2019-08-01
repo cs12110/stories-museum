9af2839e70f67c686e61c6b76add582ec2620e35
huanghuapeng <huanghuapeng@ingbaobei.com>
2019-08-01 11:26:19 +0800

    make some fun

--- stories-museum-service/src/main/java/com/pkgs/museum/ctrl/MuseumCtrl.java
+++ stories-museum-service/src/main/java/com/pkgs/museum/ctrl/MuseumCtrl.java
@@ -2,6 +2,7 @@ package com.pkgs.museum.ctrl;
 
 import com.alibaba.fastjson.JSON;
 import com.pkgs.museum.handler.WxEventDispatchHandler;
+import com.pkgs.museum.util.XmlUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -41,7 +47,7 @@ public class MuseumCtrl {
      */
     @RequestMapping("/bigbang")
     @ResponseBody
-    public Object bigbang(HttpServletRequest request) {
+    public void bigbang(HttpServletRequest request, HttpServletResponse response) {
         String reqValue = try2Read(request);
         logger.info("\n{}", reqValue);
 
@@ -52,12 +58,19 @@ public class MuseumCtrl {
             map.put(key, request.getParameter(key));
         }
 
-        handler.dealWith(reqValue);
 
         logger.info("\n{}", JSON.toJSONString(map, true));
 
-        return map.get("echostr");
+        response.setCharacterEncoding("utf-8");
+        try (PrintWriter writer = response.getWriter()) {
+            String feedback = try2SendMessageToStoriesMuseum(reqValue);
+            try2Write(writer, feedback);
+            //try2Write(writer, String.valueOf(map.get("echostr")));
+        } catch (Exception e) {
+            logger.error("", e);
+        }
     }
+
 
     /**
      * 读取事件内容
@@ -77,5 +90,43 @@ public class MuseumCtrl {
             logger.error("", e);
         }
         return builder.toString();
+    }
+
+
+    private void try2Write(PrintWriter writer, String message) {
+        writer.write(message);
+        writer.flush();
+    }
+
+
+    private String try2SendMessageToStoriesMuseum(String xml) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Map<String, String> eventMap = XmlUtil.toMap(xml);
+        String toUserName = eventMap.get("FromUserName");
+        String fromUserName = eventMap.get("ToUserName");
+        String content = eventMap.get("Content");
+
+
+        String feedback = sdf.format(new Date()) + ",输入了: " + content;
+
+        String textXml = "<xml>\n" +
+                "  <ToUserName><![CDATA[" + toUserName + "]]></ToUserName>\n" +
+                "  <FromUserName><![CDATA[" + fromUserName + "]]></FromUserName>\n" +
+                "  <CreateTime>" + System.currentTimeMillis() + "</CreateTime>\n" +
+                "  <MsgType><![CDATA[text]]></MsgType>\n" +
+                "  <Content><![CDATA[" + feedback + "]]></Content>\n" +
+                "</xml>";
+
+        //try (PrintWriter writer = response.getWriter()) {
+        //    writer.write(textXml);
+        //    writer.flush();
+        //} catch (Exception e) {
+        //    logger.error("", e);
+        //}
+
+        return textXml;
+
     }
 }
